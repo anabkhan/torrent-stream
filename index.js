f5968b05289a1c728821f8129b42ad6f855e73e7
Anab Khan <akanabkhan@gmail.com>
2020-12-21 10:25:17 +0000

    logs

--- index.js
+++ index.js
@@ -570,13 +570,13 @@ var torrentStream = function (link, opts, cb) {
 
     var loop = function (i) {
       if (i >= torrent.pieces.length) return onready()
-      // engine.store.get(i, function (_, buf) {
-      //   if (!buf || sha1(buf) !== torrent.pieces[i] || !pieces[i]) return loop(i + 1)
-      //   pieces[i] = null
-      //   engine.bitfield.set(i, true)
-      //   engine.emit('verify', i)
-      //   loop(i + 1)
-      // })
+      engine.store.get(i, function (_, buf) {
+        if (!buf || sha1(buf) !== torrent.pieces[i] || !pieces[i]) return loop(i + 1)
+        pieces[i] = null
+        engine.bitfield.set(i, true)
+        engine.emit('verify', i)
+        loop(i + 1)
+      })
     }
 
     loop(0)
